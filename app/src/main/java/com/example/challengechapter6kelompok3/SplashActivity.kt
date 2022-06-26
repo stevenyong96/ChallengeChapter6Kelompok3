6cf00e889074a71a4c59c21141d82cdeed59c26f
steven <steven.yong@outlook.com>
2022-06-26 10:47:10 +0700

    first commit

---
+++ app/src/main/java/com/example/challengechapter6kelompok3/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.challengechapter6kelompok3
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000L)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
